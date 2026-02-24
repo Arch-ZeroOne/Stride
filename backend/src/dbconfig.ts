bef0b172eafd4b0c3c5a2605d157c842364dd9d6
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-02-25 04:38:27 +0800

    Added config file

--- backend/src/dbconfig.ts
+++ backend/src/dbconfig.ts
@@ -5,13 +5,11 @@ import { QueryResult } from "pg";
 dotenv.config();
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: parseInt(process.env.PG_PORT || "3000", 10),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 db.connect();
 db.on("error", (err) => {
   console.error("Unexpected error on idle client", err);
