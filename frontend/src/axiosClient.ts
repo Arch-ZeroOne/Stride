95b91d21f246167e9f51a96e59be867ac98f7046
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-02-28 00:04:03 +0800

    Clean up dashboard

--- frontend/src/axiosClient.ts
+++ frontend/src/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://stride-deployment.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 export default client;
